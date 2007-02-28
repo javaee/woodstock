52c75cb5887519c35a0d74675db5edaaf4b4e019
gjmurphy <gjmurphy@localhost>
2007-02-28 22:47:40 +0000

    Converting custom property editors for the "options" and "selected value" properties on the default options backing bean, used only at design-time, from full-feature editors that extend the VWP TabularPropertyEditor, to read-only editors that just display the current values. The motivation for the change is that it is no longer possible to extend TabularPropertyEditor in NB 6. Note that users can still modify the default bean values via the "edit default options" customizer, from the right-click context menu. Most users are of course interested in binding to real data, so the loss of these editors should not cause any grief.
    svn path=/trunk/; revision=34
    
    
    Former-commit-id: 15edeac1591b7ce40ff75cce6574ed4ddc6ea305

--- webui/src/designtime/com/sun/webui/jsf/component/propertyeditors/SelectedValuesPropertyEditor.java
+++ webui/src/designtime/com/sun/webui/jsf/component/propertyeditors/SelectedValuesPropertyEditor.java
@@ -21,19 +21,11 @@
  */
 package com.sun.webui.jsf.component.propertyeditors;
 
-import com.sun.rave.designtime.DesignBean;
-import com.sun.rave.designtime.DesignProperty;
-import com.sun.rave.propertyeditors.TabularPropertyEditor;
-import com.sun.rave.propertyeditors.TabularPropertyModel;
-import com.sun.rave.propertyeditors.util.JavaInitializer;
-import com.sun.webui.jsf.component.util.DesignMessageUtil;
-import com.sun.webui.jsf.model.MultipleSelectOptionsList;
-import com.sun.webui.jsf.model.Option;
-import java.awt.Component;
-import java.util.ArrayList;
+import java.beans.PropertyEditorSupport;
 import java.util.Arrays;
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
+import java.util.Collection;
+import java.util.Collections;
+
 
 /**
  * An editor for properties that specify the options that are "selected" among
@@ -43,249 +35,32 @@ import javax.swing.event.TableModelListener;
  *
  * @author gjmurphy
  */
-public class SelectedValuesPropertyEditor extends TabularPropertyEditor {
+public class SelectedValuesPropertyEditor extends PropertyEditorSupport {
     
-    public SelectedValuesPropertyEditor() {
+    
+    public void setAsText(String text) throws IllegalArgumentException {
+        // Read-only
     }
-    
-    private SelectedValuesTableModel tableModel;
-    
-    protected TabularPropertyModel getTabularPropertyModel() {
-        if (tableModel == null)
-            tableModel = new SelectedValuesTableModel();
-        tableModel.setDesignProperty(this.getDesignProperty());
-        return tableModel;
-    }
-    
-    public String getJavaInitializationString() {
-        StringBuffer buffer = new StringBuffer();
-        Object value = this.getValue();
-        if (value instanceof Object[]) {
-            buffer.append("new Object[] {");
-            Object[] values = (Object[]) value;
-            for (int i = 0; i < values.length; i++) {
-                if (i > 0)
-                    buffer.append(", ");
-                buffer.append(JavaInitializer.toJavaInitializationString(values[i]));
-            }
-            buffer.append("}");
-        } else {
-            buffer.append(JavaInitializer.toJavaInitializationString(value));
-        }
-        return buffer.toString();
-    }
-    
+
     public String getAsText() {
-        StringBuffer buffer = new StringBuffer();
         Object value = this.getValue();
+        if (value == null)
+            return "";
+        StringBuffer buffer = new StringBuffer();
+        Collection collection;
         if (value instanceof Object[]) {
-            Object[] values = (Object[]) value;
-            for (int i = 0; i < values.length; i++) {
-                if (i > 0)
-                    buffer.append(", ");
-                buffer.append(values[i].toString());
-            }
-        } else if (value != null) {
-            buffer.append(value.toString());
+            collection = Arrays.asList((Object[]) value);
+        } else if (value instanceof Collection) {
+            collection = (Collection) value;
+        } else {
+            collection = Collections.singletonList(value);
         }
+        for (Object item : collection) {
+            buffer.append(item.toString());
+            buffer.append(", ");
+        }
+        buffer.setLength(buffer.length() - 2);
         return buffer.toString();
-    }
-    
-    static final String[] columnNames = new String[] {
-        DesignMessageUtil.getMessage(OptionsPropertyEditor.class, "Options.label"), //NOI18N
-                DesignMessageUtil.getMessage(OptionsPropertyEditor.class, "Options.value"), //NOI18N
-                DesignMessageUtil.getMessage(OptionsPropertyEditor.class, "Options.selected") //NOI18N
-    };
-    
-    class SelectedValuesTableModel implements TabularPropertyModel {
-        
-        final int labelIndex = 0;
-        final int valueIndex = 1;
-        final int selectedIndex = 2;
-        
-        DesignProperty designProperty;
-        // List of the component options from which selections are made
-        ArrayList options;
-        // List of boolean values that reflected selected state of the options
-        ArrayList selected;
-        // If true, component allows multiple selections
-        boolean isMultiple;
-        
-        public void setValue(Object value) {
-            options = new ArrayList();
-            selected = new ArrayList();
-            if (designProperty != null) {
-                DesignBean bean = designProperty.getDesignBean();
-                Option[] opts = (Option[]) bean.getProperty("options").getValue();
-                DesignProperty multipleProperty = bean.getProperty("multiple");
-                if ((multipleProperty != null && Boolean.TRUE.equals(multipleProperty.getValue())) ||
-                        bean.getInstance() instanceof MultipleSelectOptionsList) {
-                    Object[] values = value == null ? new Object[0] : (Object[]) value;
-                    isMultiple = true;
-                    for (int i = 0; i < opts.length; i++) {
-                        options.add(opts[i]);
-                        selected.add(Boolean.FALSE);
-                        for (int j = 0; j < values.length; j++) {
-                            if (opts[i].getValue().equals(values[j]))
-                                selected.set(i, Boolean.TRUE);
-                        }
-                    }
-                } else {
-                    isMultiple = false;
-                    for (int i = 0; i < opts.length; i++) {
-                        options.add(opts[i]);
-                        if (opts[i].getValue().equals(value))
-                            selected.add(Boolean.TRUE);
-                        else
-                            selected.add(Boolean.FALSE);
-                    }
-                }
-            }
-        }
-        
-        public Object getValue() {
-            if (selected == null)
-                return null;
-            if (isMultiple) {
-                ArrayList values = new ArrayList();
-                for (int i = 0; i < selected.size(); i++) {
-                    if (Boolean.TRUE.equals(selected.get(i)))
-                        values.add(((Option) options.get(i)).getValue());
-                }
-                return values.toArray(new Object[values.size()]);
-            } else {
-                for (int i = 0; i < selected.size(); i++) {
-                    if (Boolean.TRUE.equals(selected.get(i)))
-                        return ((Option) options.get(i)).getValue();
-                }
-            }
-            return null;
-        }
-        
-        ArrayList tableModelListenerList = new ArrayList();
-        
-        public void addTableModelListener(TableModelListener listener) {
-            tableModelListenerList.add(listener);
-        }
-        
-        public void removeTableModelListener(TableModelListener listener) {
-            tableModelListenerList.remove(listener);
-        }
-        
-        public void setDesignProperty(DesignProperty designProperty) {
-            this.designProperty = designProperty;
-        }
-        
-        public boolean isCellEditable(int rowIndex, int columnIndex) {
-            if (columnIndex != selectedIndex)
-                return false;
-            if (rowIndex < 0 || rowIndex >= options.size())
-                return false;
-            return true;
-        }
-        
-        public void setValueAt(Object newValue, int rowIndex, int columnIndex) {
-            if (!isCellEditable(rowIndex, columnIndex))
-                return;
-            // If underlying property takes a list of values, update the list, and
-            // set property to new state of list.
-            if (isMultiple) {
-                selected.set(rowIndex, newValue);
-                ArrayList selectedValues = new ArrayList();
-                for (int i = 0; i < options.size(); i++) {
-                    if (Boolean.TRUE.equals(selected.get(i)))
-                        selectedValues.add(((Option) options.get(i)).getValue());
-                }
-                //this.setValue(selectedValues.toArray());
-            }
-            // If underlying property takes a single value, and user has just deselected
-            // the previously selected value, then set property value to null.
-            else if(Boolean.FALSE.equals(newValue)) {
-                selected.set(rowIndex, Boolean.FALSE);
-                //this.setValue(null);
-            }
-            // If underlying property takes a single value, and user has just selected
-            // a new option, deselect previously selected option, and set property value
-            // to new option's value.
-            else {
-                for (int i = 0; i < selected.size(); i++) {
-                    if (Boolean.TRUE.equals(selected.get(i))) {
-                        selected.set(i, Boolean.FALSE);
-                        //((TabularPropertyPanel) super.getCustomEditor()).updateTableData(i, selectedIndex);
-                        for (int j = 0; j < tableModelListenerList.size(); j++) {
-                            TableModelListener l = (TableModelListener) tableModelListenerList.get(j);
-                            l.tableChanged(new TableModelEvent(this, i, i, selectedIndex));
-                        }
-                    }
-                }
-                selected.set(rowIndex, Boolean.TRUE);
-                //this.setValue(((Option) options.get(rowIndex)).getValue());
-            }
-        }
-        
-        public Object getValueAt(int rowIndex, int columnIndex) {
-            if (rowIndex >= 0 && rowIndex < options.size()) {
-                if (columnIndex == labelIndex)
-                    return ((Option) options.get(rowIndex)).getLabel();
-                else if (columnIndex == valueIndex)
-                    return ((Option) options.get(rowIndex)).getValue();
-                else
-                    return selected.get(rowIndex);
-            }
-            return null;
-        }
-        
-        public Class getColumnClass(int columnIndex) {
-            if (columnIndex == labelIndex)
-                return String.class;
-            else if (columnIndex == selectedIndex)
-                return Boolean.class;
-            else if (options.size() > 0 && options.get(0) != null)
-                return ((Option) options.get(0)).getValue().getClass();
-            else
-                return Object.class;
-        }
-        
-        public int getRowCount() {
-            return options.size();
-        }
-        
-        public String getColumnName(int columnIndex) {
-            return columnNames[columnIndex];
-        }
-        
-        public int getColumnCount() {
-            return columnNames.length;
-        }
-        
-        public boolean canAddRow() {
-            return false;
-        }
-        
-        public boolean addRow() {
-            return false;
-        }
-        
-        public boolean canMoveRow(int indexFrom, int indexTo) {
-            return false;
-        }
-        
-        public boolean moveRow(int indexFrom, int indexTo) {
-            return false;
-        }
-        
-        public boolean canRemoveRow(int index) {
-            return false;
-        }
-        
-        public boolean removeRow(int index) {
-            return false;
-        }
-        
-        public boolean removeAllRows() {
-            return false;
-        }
-        
     }
     
 }
