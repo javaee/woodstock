2b31be392e8533bd88581e8bb141260367278248
dkushner <dkushner@localhost>
2007-05-15 13:20:07 +0000

    changes to encodeChildren to further differentiate different requests ( validate, submit, ...)
    svn path=/trunk/; revision=285
    
    
    Former-commit-id: 12a805961af49cea84d139fa038e892c4c6dae86

--- webui/src/runtime/com/sun/webui/jsf/renderkit/ajax/TextFieldRenderer.java
+++ webui/src/runtime/com/sun/webui/jsf/renderkit/ajax/TextFieldRenderer.java
@@ -41,9 +41,9 @@ import org.json.JSONObject;
  * This class responds to Ajax requests made to ProgressBar components.
  */
 @Renderer(@Renderer.Renders(
-    rendererType="com.sun.webui.jsf.ajax.TextField",
-    componentFamily="com.sun.webui.jsf.TextField"))
-public class TextFieldRenderer
+rendererType="com.sun.webui.jsf.ajax.TextField",
+        componentFamily="com.sun.webui.jsf.TextField"))
+        public class TextFieldRenderer
         extends com.sun.webui.jsf.renderkit.widget.TextFieldRenderer  {
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // Renderer methods
@@ -74,42 +74,55 @@ public class TextFieldRenderer
      * @exception NullPointerException if context or component is null.
      */
     public void encodeChildren(FacesContext context, UIComponent component)
-            throws IOException {
+    throws IOException {
         if (context == null || component == null) {
             throw new NullPointerException();
         }
-
+        
         // Output component properties if Ajax request and is refresh or submit event.
-        if (ComponentUtilities.isAjaxRequest(context, component, "refresh") ||
-            ComponentUtilities.isAjaxRequest(context, component, "submit")) {
+        if (ComponentUtilities.isAjaxRequest(context, component, "refresh") ) {
             super.encodeChildren(context, component);
         }
-
-        // Return if Ajax request and is not validate event.
-        if (!ComponentUtilities.isAjaxRequest(context, component, "validate")) {
+        
+        // "submit" request
+        if (ComponentUtilities.isAjaxRequest(context, component, "submit")) {
+            try {
+                JSONObject json = new JSONObject();
+                json.put("id", component.getClientId(context));
+                json.write(context.getResponseWriter());
+            } catch(JSONException e) {
+                e.printStackTrace();
+            }
             return;
         }
+        
+        // Return if Ajax request and is not validate event.
+        // "submit" request would return here
+        if (ComponentUtilities.isAjaxRequest(context, component, "validate")) {
             
-        try {
-            boolean valid = ((TextField) component).isValid();
-            JSONObject json = new JSONObject();
-            json.put("valid", valid);
-            json.put("id", component.getClientId(context));
+            try {
+                boolean valid = ((TextField) component).isValid();
+                JSONObject json = new JSONObject();
+                json.put("valid", valid);
+                json.put("id", component.getClientId(context));
                 
-            if (!valid) {
-                Iterator msgs = context.getMessages(component.getClientId(context));
-                if (msgs.hasNext()) {
-                    FacesMessage msg = (FacesMessage) msgs.next();
-                    json.put("detail", msg.getDetail());
-                    json.put("summary", msg.getSummary());
-                    json.put("severity", msg.getSeverity());
+                if (!valid) {
+                    Iterator msgs = context.getMessages(component.getClientId(context));
+                    if (msgs.hasNext()) {
+                        FacesMessage msg = (FacesMessage) msgs.next();
+                        json.put("detail", msg.getDetail());
+                        json.put("summary", msg.getSummary());
+                        json.put("severity", msg.getSeverity());
+                    }
                 }
+                json.write(context.getResponseWriter());
+            } catch(JSONException e) {
+                e.printStackTrace();
             }
-            json.write(context.getResponseWriter());
-        } catch(JSONException e) {
-            e.printStackTrace();
+            return;
         }
-    }   
+        
+    }
     
     /**
      * Render the ending of the specified UIComponent to the output stream or
