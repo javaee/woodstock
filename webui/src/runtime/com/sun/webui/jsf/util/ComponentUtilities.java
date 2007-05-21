840707365021d411cbb5f8e24b7eeb896124dbb6
dkushner <dkushner@localhost>
2007-05-21 19:33:15 +0000

    added isAjaxExecuteRequest replaced NPE into 'return false' for isAjaxRequest
    svn path=/trunk/; revision=310
    
    
    Former-commit-id: 58dcb1de96b63403076901d9f7b5a19f66e1f527

--- webui/src/runtime/com/sun/webui/jsf/util/ComponentUtilities.java
+++ webui/src/runtime/com/sun/webui/jsf/util/ComponentUtilities.java
@@ -3,12 +3,12 @@
  * of the Common Development and Distribution License
  * (the License).  You may not use this file except in
  * compliance with the License.
- * 
+ *
  * You can obtain a copy of the license at
  * https://woodstock.dev.java.net/public/CDDLv1.0.html.
  * See the License for the specific language governing
  * permissions and limitations under the License.
- * 
+ *
  * When distributing Covered Code, include this CDDL
  * Header Notice in each file and include the License file
  * at https://woodstock.dev.java.net/public/CDDLv1.0.html.
@@ -16,12 +16,12 @@
  * with the fields enclosed by brackets [] replaced by
  * you own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * Copyright 2007 Sun Microsystems, Inc. All rights reserved.
  */
 
 /*
- * $Id: ComponentUtilities.java,v 1.5 2007-04-18 04:22:18 danl Exp $
+ * $Id: ComponentUtilities.java,v 1.6 2007-05-21 19:33:15 dkushner Exp $
  */
 
 package com.sun.webui.jsf.util;
@@ -29,6 +29,7 @@ package com.sun.webui.jsf.util;
 import com.sun.faces.extensions.avatar.lifecycle.AsyncResponse;
 
 import java.util.Map;
+import java.util.StringTokenizer;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -40,15 +41,15 @@ import org.json.JSONObject;
  * Methods for general component manipulation.
  */
 public class ComponentUtilities {
-
+    
     private final static String USCORE = "_"; //NOI18N
-
+    
     /** Creates a new instance of ComponentUtilities. */
     public ComponentUtilities() {
     }
-
+    
     /**
-     * Store an internally created component utilizing the 
+     * Store an internally created component utilizing the
      * internal facet naming convention by mapping the facet
      * to the name returned by <code>createPrivateFacetName()</code>.
      * Add the component to the parent's facets map.
@@ -58,16 +59,16 @@ public class ComponentUtilities {
      * @param facet the private facet component instance
      */
     public static void putPrivateFacet(UIComponent parent,
-	    String facetName, UIComponent facet) {
-
-	if (parent == null || facet == null || facetName == null) {
-	    return;
-	}
-	parent.getFacets().put(createPrivateFacetName(facetName), facet);
+            String facetName, UIComponent facet) {
+        
+        if (parent == null || facet == null || facetName == null) {
+            return;
+        }
+        parent.getFacets().put(createPrivateFacetName(facetName), facet);
     }
-
+    
     /**
-     * Remove an internally created component utilizing the 
+     * Remove an internally created component utilizing the
      * internal facet naming convention by mapping the facet
      * to the name returned by <code>createPrivateFacetName()</code>.
      * Remove the component from the parent's facets map.
@@ -76,12 +77,12 @@ public class ComponentUtilities {
      * @param facetName the public facet name
      */
     public static void removePrivateFacet(UIComponent parent,
-	    String facetName) {
-
-	if (parent == null || facetName == null) {
-	    return;
-	}
-	parent.getFacets().remove(createPrivateFacetName(facetName));
+            String facetName) {
+        
+        if (parent == null || facetName == null) {
+            return;
+        }
+        parent.getFacets().remove(createPrivateFacetName(facetName));
     }
     /**
      * Return a private facet from the the parent component's facet map.
@@ -89,7 +90,7 @@ public class ComponentUtilities {
      * <code>createPrivateFacetName()</code> on the facetName parameter.
      * <p>
      * If the matchId parameter is true, verify that the facet that is found
-     * has an id that matches the value of  
+     * has an id that matches the value of
      * <code>getPrivateFacetId(parent.getId(), facetName)</code>.
      * If the id's do not match return null and remove the existing facet.</br>
      * If matchId is false, return the facet if found or null.
@@ -100,32 +101,32 @@ public class ComponentUtilities {
      * @return a UIComponent if the facet is found else null.
      */
     public static UIComponent getPrivateFacet(UIComponent parent,
-	    String facetName, boolean matchId) {
-
-	if (parent == null || facetName == null) {
-	    return null;
-	}
-
-	String pfacetName = createPrivateFacetName(facetName);
-	UIComponent facet = (UIComponent)parent.getFacets().get(pfacetName);
-	if (facet == null) {
-	    return null;
-	}
-
-	if (matchId == false) {
-	    return facet;
-	}
-
-	// Will never be null as long as facetName is not null.
-	//
-	String id = createPrivateFacetId(parent, facetName);
-	if (!id.equals(facet.getId())) {
-	    parent.getFacets().remove(pfacetName);
-	    return null;
-	}
-	return facet;
+            String facetName, boolean matchId) {
+        
+        if (parent == null || facetName == null) {
+            return null;
+        }
+        
+        String pfacetName = createPrivateFacetName(facetName);
+        UIComponent facet = (UIComponent)parent.getFacets().get(pfacetName);
+        if (facet == null) {
+            return null;
+        }
+        
+        if (matchId == false) {
+            return facet;
+        }
+        
+        // Will never be null as long as facetName is not null.
+        //
+        String id = createPrivateFacetId(parent, facetName);
+        if (!id.equals(facet.getId())) {
+            parent.getFacets().remove(pfacetName);
+            return null;
+        }
+        return facet;
     }
-
+    
     /**
      * Prefix the facetName parameter with an "_".
      *
@@ -133,9 +134,9 @@ public class ComponentUtilities {
      * @return a private facet name
      */
     public static String createPrivateFacetName(String facetName) {
-	return USCORE.concat(facetName);
+        return USCORE.concat(facetName);
     }
-
+    
     /**
      * Return an id using the convention</br>
      * <code>parent.getId() + "_" + facetName</code>
@@ -147,14 +148,14 @@ public class ComponentUtilities {
      * @return an id for a private facet.
      */
     public static String createPrivateFacetId(UIComponent parent,
-	    String facetName) {
-
-	String pfacetName = createPrivateFacetName(facetName);
-	String id = parent.getId();
-	if (id != null) {
-	    pfacetName = id.concat(pfacetName);
-	}
-	return pfacetName;
+            String facetName) {
+        
+        String pfacetName = createPrivateFacetName(facetName);
+        String id = parent.getId();
+        if (id != null) {
+            pfacetName = id.concat(pfacetName);
+        }
+        return pfacetName;
     }
     
     /**
@@ -162,7 +163,7 @@ public class ComponentUtilities {
      *
      * @param component <code>UIComponent</code> to be checked
      */
-    public static boolean isDisabled(UIComponent component) {        
+    public static boolean isDisabled(UIComponent component) {
         Object disabled = component.getAttributes().get("disabled");
         if (disabled == null) {
             return (false);
@@ -171,15 +172,15 @@ public class ComponentUtilities {
             return (Boolean.valueOf((String) disabled).booleanValue());
         } else {
             return (disabled.equals(Boolean.TRUE));
-        }        
-    }    
+        }
+    }
     
     /**
      * <p>Return <code>true</code> if the specified component is read only.</p>
      *
      * @param component <code>UIComponent</code> to be checked
      */
-    public static boolean isReadOnly(UIComponent component) {        
+    public static boolean isReadOnly(UIComponent component) {
         Object readonly = component.getAttributes().get("readonly");
         if (readonly == null) {
             return (false);
@@ -188,9 +189,9 @@ public class ComponentUtilities {
             return (Boolean.valueOf((String) readonly).booleanValue());
         } else {
             return (readonly.equals(Boolean.TRUE));
-        }        
+        }
     }
-
+    
     /**
      * Return <code>true</code> if the Ajax request is for the given component.
      *
@@ -201,7 +202,7 @@ public class ComponentUtilities {
             UIComponent component) {
         return isAjaxRequest(context, component, null);
     }
-
+    
     /**
      * Return <code>true</code> if the Ajax request is for the given component
      * and matches the given event name.
@@ -209,23 +210,24 @@ public class ComponentUtilities {
      * @param context FacesContext for the current request.
      * @param component UIComponent to be rendered.
      * @param event The event name for this Ajax request (may be null).
-     * @exception NullPointerException if <code>context</code> or
-     *  <code>component</code> is <code>null</code>
+     * @return  
+     *      <code>true</code> if the Ajax request is for the given component
+     *  and matches the given event name, or 
+     *      <code>false</code> otherwise
      */
     public static boolean isAjaxRequest(FacesContext context,
             UIComponent component, String event) {
-        if (context == null || component == null) {
-            throw new NullPointerException();
-        }
+        if (context == null || component == null) 
+            return false;
         boolean isAjaxRequest = false;
-
+        
         // Ensure this request is not for an AjaxZone.
         if (AsyncResponse.isAjaxRequest()) {
             try {
                 Map map = context.getExternalContext().getRequestHeaderMap();
                 JSONObject xjson = new JSONObject((String)
-                    map.get(AsyncResponse.XJSON_HEADER));
-
+                map.get(AsyncResponse.XJSON_HEADER));
+                
                 String id = (String) xjson.get("id");
                 if (component.getClientId(context).equals(id)) {
                     if (event != null) {
@@ -241,5 +243,43 @@ public class ComponentUtilities {
             } catch(NullPointerException e) {} // JSON property may be null.
         }
         return isAjaxRequest;
+    }
+    
+    /**
+     * Return <code>true</code> if the Ajax request is for the given component
+     * and component's id is present in the list of components to execute.
+     * This method assumes that request contains a list of ids to be executed under
+     * AsyncResponse.EXECUTE_HEADER key. If such list is not found, method returns
+     * false.
+     *
+     * @param context FacesContext for the current request.
+     * @param component UIComponent to be rendered.
+     * @return  
+     *      <code>true</code> if the Ajax request is for the given component
+     * and component's id is present in the list of components to execute.
+     *      <code>false</code> otherwise
+     */
+    public static boolean isAjaxExecuteRequest(FacesContext context,
+            UIComponent component) {
+        if (context == null || component == null) 
+            return false;
+        
+        // Ensure this request is not for an AjaxZone.
+        if (AsyncResponse.isAjaxRequest()) {
+            Map<String,String> map = context.getExternalContext().getRequestHeaderMap();
+            String listToExecute = map.get(AsyncResponse.EXECUTE_HEADER);
+            if (listToExecute == null || listToExecute.equals("none"))
+                return false;
+            
+            String compId = component.getClientId(context);
+            //fireAjaxTransaction syntax calls for comma-separted list of ids
+            //thereby we tokenize by " " and ","
+            StringTokenizer st = new StringTokenizer(listToExecute, ", ");
+            while (st.hasMoreTokens()) {
+                if (st.nextToken().equals(compId))
+                    return true;
+            }            
+        }
+        return false;
     }
 }
