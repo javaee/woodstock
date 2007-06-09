fd6312b3d4ce382bcda74ed186e22cc8f56d48d4
gjmurphy <gjmurphy@localhost>
2007-06-09 14:01:26 +0000

    Fix for NetBeans VisualWebPack issu 99560.
    Since the FilterCriteria class is annotated as being a component, it is included
    in the faces-config file and marked as being a component. But the class does not
    extend UIComponent, hence the exception.
    
    The best solution to this problem is to refactor the annotations library into
    two, one just for beans, so that design-time for classes like this
    FilterCriteria can be generated without its being declared a run-time component.
    
    Work-around for now: remove the annotations from all FilterCriteria classes, and
    hand-code their BeanInfo.
    
    svn path=/trunk/; revision=365
    
    
    Former-commit-id: df966b7efab47991c97f580301173cdedac8c5a8

--- webui/src/designtime/com/sun/webui/jsf/faces/ValueBindingSortCriteriaBeanInfo.java
+++ webui/src/designtime/com/sun/webui/jsf/faces/ValueBindingSortCriteriaBeanInfo.java
@@ -21,10 +21,237 @@
  */
 package com.sun.webui.jsf.faces;
 
-/**
- *
- * @author Joe Nuxoll
- */
-public class ValueBindingSortCriteriaBeanInfo extends ValueBindingSortCriteriaBeanInfoBase {
-    ;
+import java.awt.Image;
+import java.beans.*;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
+import com.sun.rave.designtime.CategoryDescriptor;
+import com.sun.rave.designtime.Constants;
+import com.sun.rave.designtime.faces.FacetDescriptor;
+import com.sun.rave.designtime.markup.AttributeDescriptor;
+
+
+abstract class ValueBindingSortCriteriaBeanInfo extends SimpleBeanInfo {
+    
+    /**
+     * The class of the component (bean) to which this BeanInfo corresponds.
+     */
+    protected Class beanClass = com.sun.webui.jsf.faces.ValueBindingSortCriteria.class;
+
+    protected static ResourceBundle resourceBundle =
+        ResourceBundle.getBundle("com.sun.webui.jsf.faces.BeanInfoBundle", Locale.getDefault(),ValueBindingSortCriteriaBeanInfo.class.getClassLoader());
+    
+    private BeanDescriptor beanDescriptor;
+    
+
+    /**
+     * Return the <code>BeanDescriptor</code> for this bean.
+     */
+    public BeanDescriptor getBeanDescriptor() {
+
+        if (beanDescriptor == null) {
+            beanDescriptor = new BeanDescriptor(this.beanClass);
+            beanDescriptor.setDisplayName(resourceBundle.getString("ValueBindingSortCriteria_displayName"));
+            beanDescriptor.setShortDescription(resourceBundle.getString("ValueBindingSortCriteria_shortDescription"));
+            beanDescriptor.setValue(Constants.BeanDescriptor.FACET_DESCRIPTORS,
+                    this.getFacetDescriptors());
+            beanDescriptor.setValue(Constants.BeanDescriptor.HELP_KEY, "");
+            beanDescriptor.setValue(Constants.BeanDescriptor.INSTANCE_NAME, "valueBindingSortCriteria");
+            beanDescriptor.setValue(Constants.BeanDescriptor.PROPERTIES_HELP_KEY,"");
+            beanDescriptor.setValue(Constants.BeanDescriptor.PROPERTY_CATEGORIES,
+                    this.getCategoryDescriptors());
+        }
+        return beanDescriptor;
+        
+    }
+    
+    
+    private int defaultPropertyIndex = -2;
+    
+    /**
+     * Return the index of the default property, or -1 if there is no default property.
+     */
+    public int getDefaultPropertyIndex() {
+        if (defaultPropertyIndex == -2) {
+            defaultPropertyIndex = -1;
+        }
+        return defaultPropertyIndex;
+    }
+    
+    private int defaultEventIndex = -2;
+    
+    /**
+     * Return the index of the default event, or -1 if there is no default event.
+     */
+    public int getDefaultEventIndex() {
+        if (defaultEventIndex == -2) {
+            defaultEventIndex = -1;
+        }
+        return defaultEventIndex;
+    }
+
+    private CategoryDescriptor[] categoryDescriptors;
+    
+    /**
+     * Returns an array of <code>CategoryDescriptor</code>s, representing all
+     * property categories referenced by properties of this component.
+     */
+    protected CategoryDescriptor[] getCategoryDescriptors() {
+        if (categoryDescriptors == null) {
+            categoryDescriptors = new CategoryDescriptor[]{
+            };
+        }
+        return categoryDescriptors;
+    }
+    
+    private FacetDescriptor[] facetDescriptors;
+    
+    /**
+     * Returns an array of <code>FacetDescriptor</code>s for the component.
+     */
+    public FacetDescriptor[] getFacetDescriptors() {
+        if (facetDescriptors == null)
+            facetDescriptors = new FacetDescriptor[] {};
+        return facetDescriptors;
+    }
+    
+    
+    // The 16x16 color icon.
+    protected String iconFileName_C16 = "ValueBindingSortCriteria_C16";
+    
+    // The 32x32 color icon.
+    private String iconFileName_C32 = "ValueBindingSortCriteria_C32";
+    
+    // The 16x16 monochrome icon.
+    private String iconFileName_M16 = "ValueBindingSortCriteria_M16";
+    
+    // The 32x32 monochrome icon.
+    private String iconFileName_M32 = "ValueBindingSortCriteria_C32";
+    
+    /**
+     * Returns an appropriate image icon (if any) for the component.
+     */
+    public Image getIcon(int kind) {
+        String name;
+        switch (kind) {
+            case ICON_COLOR_16x16:
+                name = iconFileName_C16;
+                break;
+            case ICON_COLOR_32x32:
+                name = iconFileName_C32;
+                break;
+            case ICON_MONO_16x16:
+                name = iconFileName_M16;
+                break;
+            case ICON_MONO_32x32:
+                name = iconFileName_M32;
+                break;
+            default:
+                name = null;
+                break;
+        }
+        if (name == null)
+            return null;
+        Image image = loadImage(name + ".png");
+        if (image == null)
+            image = loadImage(name + ".gif");
+        return image;
+        
+    }
+    
+    
+    private PropertyDescriptor[] propertyDescriptors;
+    
+    /**
+     * Returns the <code>PropertyDescriptor</code>s for this component.
+     */
+    public PropertyDescriptor[] getPropertyDescriptors() {
+
+        if (propertyDescriptors == null) {
+            try {
+                Map<String,PropertyDescriptor> propertyDescriptorMap = new HashMap<String,PropertyDescriptor>();
+                PropertyDescriptor propertyDescriptor;
+                AttributeDescriptor attributeDescriptor;
+                
+
+                propertyDescriptor =
+                    new PropertyDescriptor("valueExpression", this.beanClass, "getValueExpression", "setValueExpression");
+                propertyDescriptor.setDisplayName(resourceBundle.getString("ValueBindingSortCriteria_valueExpression_displayName"));
+                propertyDescriptor.setShortDescription(resourceBundle.getString("ValueBindingSortCriteria_valueExpression_shortDescription"));
+                propertyDescriptorMap.put(propertyDescriptor.getName(), propertyDescriptor);
+
+                propertyDescriptor =
+                    new PropertyDescriptor("requestMapKey", this.beanClass, "getRequestMapKey", "setRequestMapKey");
+                propertyDescriptor.setDisplayName(resourceBundle.getString("ValueBindingSortCriteria_requestMapKey_displayName"));
+                propertyDescriptor.setShortDescription(resourceBundle.getString("ValueBindingSortCriteria_requestMapKey_shortDescription"));
+                 attributeDescriptor = new AttributeDescriptor("requestMapKey",false,null,true);
+                propertyDescriptor.setValue(Constants.PropertyDescriptor.ATTRIBUTE_DESCRIPTOR, attributeDescriptor);
+                propertyDescriptorMap.put(propertyDescriptor.getName(), propertyDescriptor);
+
+                // Property declaration inherited from com.sun.data.provider.SortCriteria
+                propertyDescriptor =
+                    new PropertyDescriptor("criteriaKey", this.beanClass, "getCriteriaKey", null);
+                propertyDescriptor.setDisplayName(resourceBundle.getString("SortCriteria_criteriaKey_displayName"));
+                propertyDescriptor.setShortDescription(resourceBundle.getString("SortCriteria_criteriaKey_shortDescription"));
+                propertyDescriptorMap.put(propertyDescriptor.getName(), propertyDescriptor);
+
+                // Property declaration inherited from com.sun.data.provider.SortCriteria
+                propertyDescriptor =
+                    new PropertyDescriptor("class", this.beanClass, "getClass", null);
+                propertyDescriptor.setDisplayName(resourceBundle.getString("SortCriteria_class_displayName"));
+                propertyDescriptor.setShortDescription(resourceBundle.getString("SortCriteria_class_shortDescription"));
+                propertyDescriptorMap.put(propertyDescriptor.getName(), propertyDescriptor);
+
+                // Property declaration inherited from com.sun.data.provider.SortCriteria
+                propertyDescriptor =
+                    new PropertyDescriptor("ascending", this.beanClass, "isAscending", "setAscending");
+                propertyDescriptor.setDisplayName(resourceBundle.getString("SortCriteria_ascending_displayName"));
+                propertyDescriptor.setShortDescription(resourceBundle.getString("SortCriteria_ascending_shortDescription"));
+                propertyDescriptorMap.put(propertyDescriptor.getName(), propertyDescriptor);
+
+                // Property declaration inherited from com.sun.data.provider.SortCriteria
+                propertyDescriptor =
+                    new PropertyDescriptor("displayName", this.beanClass, "getDisplayName", "setDisplayName");
+                propertyDescriptor.setDisplayName(resourceBundle.getString("SortCriteria_displayName_displayName"));
+                propertyDescriptor.setShortDescription(resourceBundle.getString("SortCriteria_displayName_shortDescription"));
+                propertyDescriptorMap.put(propertyDescriptor.getName(), propertyDescriptor);
+
+   
+                Collection<PropertyDescriptor> propertyDescriptorCollection = 
+                    propertyDescriptorMap.values();
+                propertyDescriptors =
+                    propertyDescriptorCollection.toArray(new PropertyDescriptor[propertyDescriptorCollection.size()]);
+
+            } catch (IntrospectionException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+         return propertyDescriptors;
+    }
+
+    private EventSetDescriptor[] eventSetDescriptors;
+
+    public EventSetDescriptor[] getEventSetDescriptors() {
+        return eventSetDescriptors;
+    }
+    
+    /**
+     * Utility method that returns a class loaded by name via the class loader that 
+     * loaded this class.
+     */
+    private Class loadClass(java.lang.String name) {
+        try {
+            return Class.forName(name);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
 }
--- webui/src/designtime/com/sun/webui/jsf/faces/ValueExpressionFilterCriteriaBeanInfo.java
+++ webui/src/designtime/com/sun/webui/jsf/faces/ValueExpressionFilterCriteriaBeanInfo.java
@@ -21,10 +21,240 @@
  */
 package com.sun.webui.jsf.faces;
 
-/**
- *
- * @author Joe Nuxoll
- */
-public class ValueExpressionFilterCriteriaBeanInfo extends ValueExpressionFilterCriteriaBeanInfoBase {
-    ;
+import java.awt.Image;
+import java.beans.*;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
+import com.sun.rave.designtime.CategoryDescriptor;
+import com.sun.rave.designtime.Constants;
+import com.sun.rave.designtime.faces.FacetDescriptor;
+import com.sun.rave.designtime.markup.AttributeDescriptor;
+
+
+abstract class ValueExpressionFilterCriteriaBeanInfo extends SimpleBeanInfo {
+    
+    /**
+     * The class of the component (bean) to which this BeanInfo corresponds.
+     */
+    protected Class beanClass = com.sun.webui.jsf.faces.ValueExpressionFilterCriteria.class;
+
+    protected static ResourceBundle resourceBundle =
+        ResourceBundle.getBundle("com.sun.webui.jsf.faces.BeanInfoBundle", Locale.getDefault(),ValueExpressionFilterCriteriaBeanInfo.class.getClassLoader());
+    
+    private BeanDescriptor beanDescriptor;
+    
+
+    /**
+     * Return the <code>BeanDescriptor</code> for this bean.
+     */
+    public BeanDescriptor getBeanDescriptor() {
+
+        if (beanDescriptor == null) {
+            beanDescriptor = new BeanDescriptor(this.beanClass);
+            beanDescriptor.setDisplayName(resourceBundle.getString("ValueExpressionFilterCriteria_displayName"));
+            beanDescriptor.setShortDescription(resourceBundle.getString("ValueExpressionFilterCriteria_shortDescription"));
+            beanDescriptor.setValue(Constants.BeanDescriptor.FACET_DESCRIPTORS,
+                    this.getFacetDescriptors());
+            beanDescriptor.setValue(Constants.BeanDescriptor.HELP_KEY, "");
+            beanDescriptor.setValue(Constants.BeanDescriptor.INSTANCE_NAME, "valueExpressionFilterCriteria");
+            beanDescriptor.setValue(Constants.BeanDescriptor.PROPERTIES_HELP_KEY,"");
+            beanDescriptor.setValue(Constants.BeanDescriptor.PROPERTY_CATEGORIES,
+                    this.getCategoryDescriptors());
+        }
+        return beanDescriptor;
+        
+    }
+    
+    
+    private int defaultPropertyIndex = -2;
+    
+    /**
+     * Return the index of the default property, or -1 if there is no default property.
+     */
+    public int getDefaultPropertyIndex() {
+        if (defaultPropertyIndex == -2) {
+            defaultPropertyIndex = -1;
+        }
+        return defaultPropertyIndex;
+    }
+    
+    private int defaultEventIndex = -2;
+    
+    /**
+     * Return the index of the default event, or -1 if there is no default event.
+     */
+    public int getDefaultEventIndex() {
+        if (defaultEventIndex == -2) {
+            defaultEventIndex = -1;
+        }
+        return defaultEventIndex;
+    }
+
+    private CategoryDescriptor[] categoryDescriptors;
+    
+    /**
+     * Returns an array of <code>CategoryDescriptor</code>s, representing all
+     * property categories referenced by properties of this component.
+     */
+    protected CategoryDescriptor[] getCategoryDescriptors() {
+        if (categoryDescriptors == null) {
+            categoryDescriptors = new CategoryDescriptor[]{
+            };
+        }
+        return categoryDescriptors;
+    }
+    
+    private FacetDescriptor[] facetDescriptors;
+    
+    /**
+     * Returns an array of <code>FacetDescriptor</code>s for the component.
+     */
+    public FacetDescriptor[] getFacetDescriptors() {
+        if (facetDescriptors == null)
+            facetDescriptors = new FacetDescriptor[] {};
+        return facetDescriptors;
+    }
+    
+    
+    // The 16x16 color icon.
+    protected String iconFileName_C16 = "ValueExpressionFilterCriteria_C16";
+    
+    // The 32x32 color icon.
+    private String iconFileName_C32 = "ValueExpressionFilterCriteria_C32";
+    
+    // The 16x16 monochrome icon.
+    private String iconFileName_M16 = "ValueExpressionFilterCriteria_M16";
+    
+    // The 32x32 monochrome icon.
+    private String iconFileName_M32 = "ValueExpressionFilterCriteria_C32";
+    
+    /**
+     * Returns an appropriate image icon (if any) for the component.
+     */
+    public Image getIcon(int kind) {
+        String name;
+        switch (kind) {
+            case ICON_COLOR_16x16:
+                name = iconFileName_C16;
+                break;
+            case ICON_COLOR_32x32:
+                name = iconFileName_C32;
+                break;
+            case ICON_MONO_16x16:
+                name = iconFileName_M16;
+                break;
+            case ICON_MONO_32x32:
+                name = iconFileName_M32;
+                break;
+            default:
+                name = null;
+                break;
+        }
+        if (name == null)
+            return null;
+        Image image = loadImage(name + ".png");
+        if (image == null)
+            image = loadImage(name + ".gif");
+        return image;
+        
+    }
+    
+    
+    private PropertyDescriptor[] propertyDescriptors;
+    
+    /**
+     * Returns the <code>PropertyDescriptor</code>s for this component.
+     */
+    public PropertyDescriptor[] getPropertyDescriptors() {
+
+        if (propertyDescriptors == null) {
+            try {
+                Map<String,PropertyDescriptor> propertyDescriptorMap = new HashMap<String,PropertyDescriptor>();
+                PropertyDescriptor propertyDescriptor;
+                AttributeDescriptor attributeDescriptor;
+                
+
+                propertyDescriptor =
+                    new PropertyDescriptor("valueExpression", this.beanClass, "getValueExpression", "setValueExpression");
+                propertyDescriptor.setDisplayName(resourceBundle.getString("ValueExpressionFilterCriteria_valueExpression_displayName"));
+                propertyDescriptor.setShortDescription(resourceBundle.getString("ValueExpressionFilterCriteria_valueExpression_shortDescription"));
+                 attributeDescriptor = new AttributeDescriptor("valueExpression",false,null,true);
+                propertyDescriptor.setValue(Constants.PropertyDescriptor.ATTRIBUTE_DESCRIPTOR, attributeDescriptor);
+                propertyDescriptorMap.put(propertyDescriptor.getName(), propertyDescriptor);
+
+                propertyDescriptor =
+                    new PropertyDescriptor("requestMapKey", this.beanClass, "getRequestMapKey", "setRequestMapKey");
+                propertyDescriptor.setDisplayName(resourceBundle.getString("ValueExpressionFilterCriteria_requestMapKey_displayName"));
+                propertyDescriptor.setShortDescription(resourceBundle.getString("ValueExpressionFilterCriteria_requestMapKey_shortDescription"));
+                 attributeDescriptor = new AttributeDescriptor("requestMapKey",false,null,true);
+                propertyDescriptor.setValue(Constants.PropertyDescriptor.ATTRIBUTE_DESCRIPTOR, attributeDescriptor);
+                propertyDescriptorMap.put(propertyDescriptor.getName(), propertyDescriptor);
+
+                propertyDescriptor =
+                    new PropertyDescriptor("compareValue", this.beanClass, "getCompareValue", "setCompareValue");
+                propertyDescriptor.setDisplayName(resourceBundle.getString("ValueExpressionFilterCriteria_compareValue_displayName"));
+                propertyDescriptor.setShortDescription(resourceBundle.getString("ValueExpressionFilterCriteria_compareValue_shortDescription"));
+                 attributeDescriptor = new AttributeDescriptor("compareValue",false,null,true);
+                propertyDescriptor.setValue(Constants.PropertyDescriptor.ATTRIBUTE_DESCRIPTOR, attributeDescriptor);
+                propertyDescriptorMap.put(propertyDescriptor.getName(), propertyDescriptor);
+
+                // Property declaration inherited from com.sun.data.provider.FilterCriteria
+                propertyDescriptor =
+                    new PropertyDescriptor("class", this.beanClass, "getClass", null);
+                propertyDescriptor.setDisplayName(resourceBundle.getString("FilterCriteria_class_displayName"));
+                propertyDescriptor.setShortDescription(resourceBundle.getString("FilterCriteria_class_shortDescription"));
+                propertyDescriptorMap.put(propertyDescriptor.getName(), propertyDescriptor);
+
+                // Property declaration inherited from com.sun.data.provider.FilterCriteria
+                propertyDescriptor =
+                    new PropertyDescriptor("include", this.beanClass, "isInclude", "setInclude");
+                propertyDescriptor.setDisplayName(resourceBundle.getString("FilterCriteria_include_displayName"));
+                propertyDescriptor.setShortDescription(resourceBundle.getString("FilterCriteria_include_shortDescription"));
+                propertyDescriptorMap.put(propertyDescriptor.getName(), propertyDescriptor);
+
+                // Property declaration inherited from com.sun.data.provider.FilterCriteria
+                propertyDescriptor =
+                    new PropertyDescriptor("displayName", this.beanClass, "getDisplayName", "setDisplayName");
+                propertyDescriptor.setDisplayName(resourceBundle.getString("FilterCriteria_displayName_displayName"));
+                propertyDescriptor.setShortDescription(resourceBundle.getString("FilterCriteria_displayName_shortDescription"));
+                propertyDescriptorMap.put(propertyDescriptor.getName(), propertyDescriptor);
+
+   
+                Collection<PropertyDescriptor> propertyDescriptorCollection = 
+                    propertyDescriptorMap.values();
+                propertyDescriptors =
+                    propertyDescriptorCollection.toArray(new PropertyDescriptor[propertyDescriptorCollection.size()]);
+
+            } catch (IntrospectionException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+         return propertyDescriptors;
+    }
+
+    private EventSetDescriptor[] eventSetDescriptors;
+
+    public EventSetDescriptor[] getEventSetDescriptors() {
+        return eventSetDescriptors;
+    }
+    
+    /**
+     * Utility method that returns a class loaded by name via the class loader that 
+     * loaded this class.
+     */
+    private Class loadClass(java.lang.String name) {
+        try {
+            return Class.forName(name);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
 }
