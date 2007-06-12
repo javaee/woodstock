1413ccedb8d12ee070ee1d4573037e65d948b8c4
dkushner <dkushner@localhost>
2007-06-12 19:41:48 +0000

    issue 241 - has to extend from EditableBeanInfoBase
    svn path=/trunk/; revision=377
    
    
    Former-commit-id: b74f8266020797d92e62c4076710119f9fa60e7b

--- webui/src/designtime/com/sun/webui/jsf/component/EditableFieldBeanInfo.java
+++ webui/src/designtime/com/sun/webui/jsf/component/EditableFieldBeanInfo.java
@@ -22,14 +22,51 @@
 
 package com.sun.webui.jsf.component;
 
+
+import com.sun.rave.designtime.CategoryDescriptor;
+import java.beans.BeanDescriptor;
+import com.sun.rave.designtime.Constants;
+import com.sun.webui.jsf.component.util.DesignUtil;
+import com.sun.webui.jsf.design.CategoryDescriptors;
+
 /**
  * BeanInfo for the {@link com.sun.webui.jsf.component.EditableField}component.
  */
-public class EditableFieldBeanInfo  extends TextFieldBeanInfo {
+public class EditableFieldBeanInfo  extends EditableFieldBeanInfoBase  {
     
     /** Creates a new instance of EditableFieldBeanInfo */
     public EditableFieldBeanInfo() {
-        super();
+        BeanDescriptor beanDescriptor = super.getBeanDescriptor();
+        beanDescriptor.setValue(
+                Constants.BeanDescriptor.INLINE_EDITABLE_PROPERTIES,
+                new String[] { "text://input", "label://label" }); // NOI18N
+        DesignUtil.updateInputEventSetDescriptors(this);
     }
+    
+    protected CategoryDescriptor[] categoryDescriptors;
+    
+    protected CategoryDescriptor[] getCategoryDescriptors() {
+        // A hack to add the category descriptor for events. Since events are not
+        // properties, they cannot be annotated with category information.
+        if (categoryDescriptors == null) {
+            CategoryDescriptor[] superCategoryDescriptors = super.getCategoryDescriptors();
+            categoryDescriptors = new CategoryDescriptor[superCategoryDescriptors.length + 1];
+            for (int i = 0, j = 0; i < superCategoryDescriptors.length; i++, j++) {
+                categoryDescriptors[j] = superCategoryDescriptors[i];
+                if (categoryDescriptors[j] == CategoryDescriptors.DATA)
+                    categoryDescriptors[++j] = CategoryDescriptors.EVENTS;
+            }
+        }
+        return categoryDescriptors;
+    }
+    
+    public BeanDescriptor getBeanDescriptor() {
+        BeanDescriptor beanDescriptor = super.getBeanDescriptor();
+        // Do not allow component to be resized.
+        beanDescriptor.setValue(Constants.BeanDescriptor.RESIZE_CONSTRAINTS,
+                new Integer(Constants.ResizeConstraints.NONE));
+        return beanDescriptor;
+    }
+    
     
 }
