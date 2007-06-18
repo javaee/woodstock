606bab1235658c8642cf8490d0b18a188fece783
wjprakash <wjprakash@localhost>
2007-06-18 20:53:25 +0000

    Issue number:  77 Obtained from: lark Submitted by:  lark Reviewed by:  rich
    svn path=/trunk/; revision=393
    
    
    Former-commit-id: 69512f0db4ae48a9705ab0aab59dae6c39ac732e

--- webui/src/runtime/com/sun/webui/jsf/component/PasswordField.java
+++ webui/src/runtime/com/sun/webui/jsf/component/PasswordField.java
@@ -130,7 +130,7 @@ public class PasswordField extends Field {
      * expression, the corresponding value will be updated
      * if validation succeeds.</p>
      */
-    @Property(name="password", displayName="Password", category="Appearance")
+    @Property(name="password", displayName="Password", category="Appearance", editorClassName="com.sun.rave.propertyeditors.StringPropertyEditor")
     public Object getPassword() {
         return getValue();
     }
