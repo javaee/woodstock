97fb7ceba871a51110051aaae4ea591af57f51cd
dkushner <dkushner@localhost>
2007-06-20 17:10:17 +0000

    PasswordField is client side rendered component now
    svn path=/trunk/; revision=402
    
    
    Former-commit-id: 44935d128a3215d2ccea131e7852f1f48f70c3f2

---
+++ themes/javascript/widget/passwordField.js
@@ -0,0 +1,71 @@
+//<!--
+// The contents of this file are subject to the terms
+// of the Common Development and Distribution License
+// (the License).  You may not use this file except in
+// compliance with the License.
+// 
+// You can obtain a copy of the license at
+// https://woodstock.dev.java.net/public/CDDLv1.0.html.
+// See the License for the specific language governing
+// permissions and limitations under the License.
+// 
+// When distributing Covered Code, include this CDDL
+// Header Notice in each file and include the License file
+// at https://woodstock.dev.java.net/public/CDDLv1.0.html.
+// If applicable, add the following below the CDDL Header,
+// with the fields enclosed by brackets [] replaced by
+// you own identifying information:
+// "Portions Copyrighted [year] [name of copyright owner]"
+// 
+// Copyright 2007 Sun Microsystems, Inc. All rights reserved.
+//
+
+dojo.provide("webui.@THEME@.widget.passwordField");
+
+dojo.require("dojo.widget.*");
+dojo.require("webui.@THEME@.*");
+dojo.require("webui.@THEME@.widget.*");
+dojo.require("webui.@THEME@.widget.field");
+
+/**
+ * This function is used to generate a template based widget.
+ *
+ * Note: This is considered a private API, do not use.
+ */
+webui.@THEME@.widget.passwordField = function() {
+    // Register widget.
+    dojo.widget.HtmlWidget.call(this);
+}
+
+
+/**
+ * Helper function to obtain widget class names.
+ */
+webui.@THEME@.widget.passwordField.getClassName = function() {
+    // Set default style.    
+    var className = (this.disabled == true)
+        ? webui.@THEME@.widget.props.passwordField.disabledClassName
+        : webui.@THEME@.widget.props.passwordField.className;
+    
+    return className;
+}
+
+
+
+// Inherit base widget properties.
+dojo.inherits(webui.@THEME@.widget.passwordField, webui.@THEME@.widget.field);
+
+// Override base widget by assigning properties to class prototype.
+dojo.lang.extend(webui.@THEME@.widget.passwordField, {
+    // Set private functions.
+    getClassName: webui.@THEME@.widget.passwordField.getClassName,
+
+    // Set defaults.
+    disabled: false,
+    required: false,
+    size: 20,
+    valid: true,
+    widgetType: "passwordField"
+});
+
+//-->
