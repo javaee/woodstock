daf3331f58709e333525abb34cf1424b56108bb4
animeshkumarsahay <animeshkumarsahay@localhost>
2008-04-11 11:27:21 +0000

    Wasn't intended to commit the changes now, bringing it to previous version.
    svn path=/trunk/; revision=1188
    
    
    Former-commit-id: f83e32e9be71e3ad9cf03e0f0eaf6b30706ac211

--- webui/src/runtime/com/sun/webui/jsf/renderkit/widget/Table2ColumnRenderer.java
+++ webui/src/runtime/com/sun/webui/jsf/renderkit/widget/Table2ColumnRenderer.java
@@ -26,10 +26,8 @@ import com.sun.faces.annotation.Renderer;
 import com.sun.webui.jsf.component.Table2Column;
 import com.sun.webui.jsf.util.JSONUtilities;
 
-import com.sun.webui.jsf.util.WidgetUtilities;
 import java.io.IOException;
 
-import java.util.Iterator;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 
@@ -118,41 +116,14 @@ public class Table2ColumnRenderer extends RendererBase {
             .put("sortLevel", col.getSortLevel())
             .put("visible", col.isVisible());
 
-        setSpanColumnProperties(context, col, json);
         // Add attributes.
         JSONUtilities.addStringProperties(stringAttributes, col, json);
         JSONUtilities.addIntegerProperties(intAttributes, col, json);
         
 
         return json;
-    }
+    }    
     
-    /** 
-     * Helper method to obtain column properties.
-     *
-     * @param context FacesContext for the current request.
-     * @param component Table2RowGroup to be rendered.
-     * @param json JSONObject to assign properties to.
-     */
-    protected void setSpanColumnProperties(FacesContext context, Table2Column component,
-            JSONObject json) throws IOException, JSONException {
-        JSONArray jArray = null;      
-
-        // Add properties for each Table2Column child.
-        Iterator kids = component.getChildren().iterator();
-        if (kids.hasNext()) {
-            jArray = new JSONArray();
-            json.put("spanCols", jArray);
-        }
-        while (kids.hasNext()) {
-            UIComponent spanCol = (UIComponent) kids.next();
-            if (spanCol instanceof Table2Column)
-             if (spanCol.isRendered()) {
-                jArray.put(WidgetUtilities.renderComponent(context, spanCol));
-            }
-        }
-    }
-
 
     /**
      * Get the type of widget represented by this component.
