35340da8550520587b89bcf63b3b30cbcde725b1
bedwards <bedwards@localhost>
2008-05-29 18:54:58 +0000

    Modified jmaki wrapper components
    svn path=/trunk/; revision=1331
    
    
    Former-commit-id: 32c3b90e8bfeeb8622391ce2d32346a9d6f287b3

--- jmaki/src/resources/button/component.js
+++ jmaki/src/resources/button/component.js
@@ -8,29 +8,14 @@ jmaki.namespace("@JMAKI_NS@.button");
  * containing valid Woodstock properties.  No property conversion
  * is done when creating the Woodstock widget from the jMaki
  * value/data properties.
- *
- * This wrapper subscribes to the "/woodstock/button" jMaki event.
- * The event callback will refresh the widget properties from the
- * value/data property in the event payload JSON object.
- *
- * This wrapper assumes the Woodstock library bootstrap has been
- * included in the page containing this button wrapper widget.
  */
 @JMAKI_NS@.button.Widget = function(wargs) {
 
     // Turn on jMaki debugging...
-    jmaki.debug = true;
-    jmaki.log("Entering woodstock button Widget function...");
-
-
-    // XXXX - Should we support a "publish" wargs property
-    //        which indicates an event to publish when the
-    //        button is clicked?  How does this interact with
-    //        the submit button?  What about a user supplied
-    //	      onClick property?
+    // jmaki.debug = true;
+    // jmaki.log("Entering woodstock button Widget function...");
 
     var self = this;
-    var refreshTopic = "/@JS_NAME@/button/refresh";
     var publishTopic;
     var id = wargs.id;
     if (typeof id == "undefined") {
@@ -43,21 +28,10 @@ jmaki.namespace("@JMAKI_NS@.button");
     }
 
     // Get the jMaki properties for a Woodstock button.
-    // If necessary, make an XHR call to read widget.json.
     var props;
     if (wargs.value) {
 	props = wargs.value;
     } else {
-	var url = wargs.widgetDir + "/widget.json";
-	var callback = function(response) {
-	    var obj = @JS_NS@.json.parse(response.responseText);
-	    props = obj.data;
-	}
-	@JS_NS@.xhr.get({url: url, onReady: callback});
-    }
-
-    // If all else fails, default to very simple button.
-    if (props == null) {
 	props = {};
 	props.value = "Button";
 	props.widgetType = "button";
@@ -76,33 +50,4 @@ jmaki.namespace("@JMAKI_NS@.button");
     // Create the Woodstock button widget.
     @JS_NS@.widget.common.createWidget(span_id, props);
 
-    // Subscribe to our refresh event.  This allows jMaki glue
-    // events to refresh our underlying Woodstock widget.
-//    jmaki.subscribe(refreshTopic, refreshHandler);
-
-    // ============================================================
-
-    // Refresh event handler for jMaki wrapper of Woodstock button.
-    //
-    // The payload argument must be a JS object containing:
-    //     {id: "widget_id", value: { ... data ...}}
-    // The data is a JS object literal containing widget properties
-    // and their updated values suitable for a setProps method.
-    //
-    // Note: This is very generic!  This will work for all widgets.
-
-    refreshHandler: function(payload) {
-
-        var id = payload.id;
-	if (id) {
-            var props = payload.value;
-	    if (props) {
-        	var widget = @JS_NS@.widget.common.getWidget(id);
-        	if (widget) {
-		    widget.setProps(props);
-		}
-	    }
-        }
-
-    };
 };
--- jmaki/src/resources/textField/component.js
+++ jmaki/src/resources/textField/component.js
@@ -1,29 +1,21 @@
 jmaki.namespace("@JMAKI_NS@.textField");
 
 /*
- * jMaki wrapper for Woodstock Button widget.
+ * jMaki wrapper for Woodstock textField widget.
  *
  * We expect the 'value' property in the wargs object and/or
  * the 'data' property in the widget.json to be a JSON object
  * containing valid Woodstock properties.  No property conversion
  * is done when creating the Woodstock widget from the jMaki
  * value/data properties.
- *
- * This wrapper subscribes to the "/woodstock/textField" jMaki event.
- * The event callback will refresh the widget properties from the
- * value/data property in the event payload JSON object.
- *
- * This wrapper assumes the Woodstock library bootstrap has been
- * included in the page containing this textField wrapper widget.
  */
 @JMAKI_NS@.textField.Widget = function(wargs) {
 
     // Turn on jMaki debugging...
-    jmaki.debug = true;
-    jmaki.log("Entering woodstock textField Widget function...");
+    // jmaki.debug = true;
+    // jmaki.log("Entering woodstock textField Widget function...");
 
     var self = this;
-    var refreshTopic = "/@JS_NAME@/textField/refresh";
     var publishTopic;
     var id = wargs.id;
     if (typeof id == "undefined") {
@@ -36,21 +28,10 @@ jmaki.namespace("@JMAKI_NS@.textField");
     }
 
     // Get the jMaki properties for a Woodstock text field.
-    // If necessary, make an XHR call to read widget.json.
     var props;
     if (wargs.value) {
 	props = wargs.value;
     } else {
-	var url = wargs.widgetDir + "/widget.json";
-	var callback = function(response) {
-	    var obj = @JS_NS@.json.parse(response.responseText);
-	    props = obj.data;
-	}
-	@JS_NS@.xhr.get({url: url, onReady: callback});
-    }
-
-    // If all else fails, default to very simple text field.
-    if (props == null) {
 	props = {};
 	props.value = "";
         props.readOnly = false;
@@ -67,33 +48,4 @@ jmaki.namespace("@JMAKI_NS@.textField");
     // Create the Woodstock textField widget.
     @JS_NS@.widget.common.createWidget(span_id, props);
 
-    // Subscribe to our refresh event.  This allows jMaki glue
-    // events to refresh our underlying Woodstock widget.
-//    jmaki.subscribe(refreshTopic, refreshHandler);
-
-    // ============================================================
-
-    // Refresh event handler for jMaki wrapper of Woodstock textField.
-    //
-    // The payload argument must be a JS object containing:
-    //     {id: "widget_id", value: { ... data ...}}
-    // The data is a JS object literal containing widget properties
-    // and their updated values suitable for a setProps method.
-    //
-    // Note: This is very generic!  This will work for all widgets.
-
-    refreshHandler: function(payload) {
-
-        var id = payload.id;
-	if (id) {
-            var props = payload.value;
-	    if (props) {
-        	var widget = @JS_NS@.widget.common.getWidget(id);
-        	if (widget) {
-		    widget.setProps(props);
-		}
-	    }
-        }
-
-    };
 };
