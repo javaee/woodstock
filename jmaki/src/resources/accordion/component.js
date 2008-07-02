4eb0c4df5af1873ca17bc486e44a184331ec276b
bedwards <bedwards@localhost>
2008-07-02 15:17:30 +0000

    Update wrappers.
    svn path=/trunk/; revision=1464
    
    
    Former-commit-id: e9a2788ffd1a0579d077f6de271a5d576fc8e456

--- jmaki/src/resources/accordion/component.js
+++ jmaki/src/resources/accordion/component.js
@@ -6,35 +6,60 @@ jmaki.namespace("@JMAKI_NS@.accordion");
  * This widget wrapper looks for the following properties in
  * the "wargs" parameter:
  *
- * value:     required (true/false) 
+ * value:     The data value following the jMaki multi-view container
+ *            data model.  Each object in the "items" array defines a
+ *            tab instance whose contents is specified as an escaped html
+ *            string ("content"), an included source file ("include"),
+ *            or a reference to another widget on the page ("refId").
+ *            Each example is shown:
+ *            {items: [
+ *              {id: <tabid>, label: <title>, content: "<html string>"},
+ *              {id: <tabid>, label: <title>, include: "<source_url>"},
+ *              {id: <tabid>, label: <title>, refId: "<widget_id>"}
+ *              ]}
+ *            The "selected" property can be set to true, which causes
+ *            that tab to be rendered open in the accordion.  If the "id"
+ *            property is omitted, it is set to the accordion id plus
+ *            "_tab<index>".  For refId, the referenced widget must be
+ *            rendered on the page before the accordion widget is rendered.
  * args:      Additional widget properties from the code snippet,
  *            these properties are assumed to be underlying widget
  *            properties and are passed through to the widget.
- * publish:   none
- * subscribe: none
- * args.id:   User specified widget identifier; if not specified, the
+ * publish:   Topic to publish jMaki events to; if not specified, the
+ *            default topic is "/woodstock/accordion".
+ * subscribe: Topic to subscribe to for data model events; if not
+ *            specified, the default topic is "/woodstock/accordion".
+ * id:        User specified widget identifier; if not specified, the
  *            jMaki auto-generated identifier is used.
  * 
  * This widget subscribes to the following jMaki events:
  *
- * /woodstock/accordion/select
- * /woodstock/accordion/setContent
- * /woodstock/accordion/setInclude
+ * select     Select a tab:
+ *            {targetId: "<tab_id>"}
+ * setContent Reset the content of a tab to an escaped html string value:
+ *            {targetId: "<tab_id>", value: "<escaped_html_string>"}
+ * setInclude Reset the content of a tab with the contents included
+ *            from a source file (containing html and/or widgets):
+ *            {targetId: "<tab_id>", value: "<source_url>"}
+ * setRefid   Reset the content of a tab to reference another widget:
+ *            {targetId: "<tab_id>", value: "<widget_identifier>"}
  *
  * This widget publishes the following jMaki events:
  * 
- * /woodstock/accordion/onSelect- to notify subscriber of selected tab
+ * onSelect   Notify subscribers that a tab has been selected
+ *            {widgetId: <wid>, type: "onSelect", targetId: "<tab_id>",
+ *             topic: "/woodstock/accordion/select"}
  * 
  */
 @JMAKI_NS@.accordion.Widget = function(wargs) {
 
     // Initialize basic wrapper properties.
-    this._subscribe = ["/woodstock/accordion/select", "/woodstock/accordion/setContent", "/woodstock/accordion/setInclude"];
-    this._publish = "/woodstock/accordion/onSelect";
+    this._subscribe = ["/woodstock/accordion"];
+    this._publish = "/woodstock/accordion";
     this._subscriptions = [];
     this._wid = wargs.uuid;
     if (wargs.id) {
-        this._wid = wargs.id;
+	this._wid = wargs.id;
     } 
     if (wargs.publish) {
 	// User supplied a specific topic to publish to.
@@ -42,220 +67,216 @@ jmaki.namespace("@JMAKI_NS@.accordion");
     }
     if (wargs.subscribe) {
 	// User supplied one or more specific topics to subscribe to.
-        if (typeof wargs.subscribe == "string") {
-            this._subscribe = [];
-            this._subscribe.push(wargs.subscribe);
-        } else {
-            this._subscribe = wargs.subscribe;
-        }
+	if (typeof wargs.subscribe == "string") {
+	    this._subscribe = [];
+	    this._subscribe.push(wargs.subscribe);
+	} else {
+	    this._subscribe = wargs.subscribe;
+	}
     }
 
     // Subscribe to jMaki events
     for (var i = 0; i < this._subscribe.length; i++) {
-        jmaki.log('subscribing to ' + this._subscribe[i]);
-        var s1 = jmaki.subscribe(this._subscribe[i] , 
-        @JS_NS@.widget.common._hitch(this, "_eventCallback"));
-        this._subscriptions.push(s1);
-
+	var s1 = jmaki.subscribe(this._subscribe[i] + "/select", 
+	@JS_NS@.widget.common._hitch(this, "_selectCallback"));
+	this._subscriptions.push(s1);
+	var s2 = jmaki.subscribe(this._subscribe[i] + "/setContent", 
+	@JS_NS@.widget.common._hitch(this, "_setContentCallback"));
+	this._subscriptions.push(s2);
+	var s3 = jmaki.subscribe(this._subscribe[i] + "/setInclude", 
+	@JS_NS@.widget.common._hitch(this, "_setIncludeCallback"));
+	this._subscriptions.push(s3);
+	var s4 = jmaki.subscribe(this._subscribe[i] + "/setRefid", 
+	@JS_NS@.widget.common._hitch(this, "_setRefidCallback"));
+	this._subscriptions.push(s4);
     }
+
     // Create Woodstock widget.
     this._create(wargs);
 };
 
 // Create Woodstock widget.
 @JMAKI_NS@.accordion.Widget.prototype._create = function(wargs) {
+
     // Get the jMaki wrapper properties for a Woodstock accordion.
     var props = {};
-    if (wargs.args) {
-	// Properties in the "args" property must be accordion properties!
-        @JS_NS@._base.proto._extend(props, wargs.args);
-        
-    } else {
-	// No data. Define minimalist accordion.
+    if (wargs.args != null) {
+	@JS_NS@._base.proto._extend(props, wargs.args);
     }
-    if (wargs.value) {
-        @JS_NS@._base.proto._extend(props, wargs.value);
-    } else {
-	// No data. Define single dummy items list.
+    if (wargs.value != null) {
+	@JS_NS@._base.proto._extend(props, wargs.value);
     }
+    this._convertModel(props);
 
     // Add our widget id and type.
     props.id = this._wid;
-     
     props.widgetType = "accordion";
 
-    this._convertModel(props);
-
-    // ============================================================
-    // Create the Woodstock widget...
-  
-      
     // Create the Woodstock accordion widget.
     var span_id = wargs.uuid + "_span";
     @JS_NS@.widget.common.createWidget(span_id, props);
        
     // add event handling
     @JS_NS@.widget.common.subscribe(@JS_NS@.widget.accordionTab.event.title.selectedTopic,
-        this, "_onTabSelected");    
+	this, "_onTabSelected");    
     
 };
 
-// Destroy...
-// Unsubscribe from jMaki events
+// Unsubscribe from jMaki events and destroy the Woodstock widget.
 @JMAKI_NS@.accordion.Widget.prototype.destroy = function() {
     if (this._subscriptions) {
-        for (var i = 0; i < this._subscriptions.length; i++) {
-            jmaki.unsubscribe(this._subscriptions[i]);
+	for (var i = 0; i < this._subscriptions.length; i++) {
+	    jmaki.unsubscribe(this._subscriptions[i]);
 	} // End of for
     }
+    @JS_NS@.widget.common.destroyWidget(this._wid);
 };
-
-
 
 // Warning: jMaki calls this function using a global scope. In order to
 // access variables and functions in "this" object, closures must be used.
 @JMAKI_NS@.accordion.Widget.prototype.postLoad = function() {
     // Do nothing...
-}
-
-// 
-// Callback function to handle all of jMaki accordion topics.
-// Event payload contains:
-//    {event: browser event}
-@JMAKI_NS@.accordion.Widget.prototype._eventCallback = function(payload) {
-    //get the event type
-    var topic = payload.topic;
-    var command = topic.substr(topic.lastIndexOf("/")+1);
-    
-    if (command == "select") {
-        this._select(payload);
-    } else if (command == "setContent") {
-        this._setContent(payload);
-    } else if (command == "setInclude") {
-        this._setInclude(payload);
-    }
-    
-};
-// select a tab
-@JMAKI_NS@.accordion.Widget.prototype._select = function(payload) {
-    if (!payload || !payload.targetId) {
-        return false;
-    }
-    var widget = @JS_NS@.widget.common.getWidget(this._wid);
-    if (widget) {
-        //widget._collapseAllTabs();
-        widget.tabSelected({id: payload.targetId});
-    }
-
 };
 
-// set a content of the tab
-@JMAKI_NS@.accordion.Widget.prototype._setContent = function(payload) {
-    if (!payload || !payload.targetId) {
-        return false;
-    }
-      
-    var widgetTab = @JS_NS@.widget.common.getWidget(payload.targetId);
-    if (widgetTab) {
-        var props = {};
-        
-        props.tabContent = [];               
-
-          var tabContentElement = {};
-          tabContentElement.html  = payload.value;
-          props.tabContent.push(tabContentElement);
-
-        //ISSUE: this will append the content, not replace it.
-        widgetTab.setProps(props);
-    }
-};
-// setInclude content of the tab
-@JMAKI_NS@.accordion.Widget.prototype._setInclude = function(payload) {
-    //we expect only payload that contains event
-    if (!payload) {
-        return false;
-    }
-      
-    var widget = @JS_NS@.widget.common.getWidget(this._wid);
-    if (widget) {
-        //
+// Callback function to handle tab selection event.
+// Event payload: {targetId: "<tab_id>"}
+@JMAKI_NS@.accordion.Widget.prototype._selectCallback = function(payload) {
+    if (payload && typeof payload.targetId == "string") {
+	this._select(payload.targetId);
     }
 };
 
+// Callback function to handle resetting tab content via escaped html string..
+// Event payload: {targetId: "<tab_id>", value: "<escaped_html_string>"}
+@JMAKI_NS@.accordion.Widget.prototype._setContentCallback = function(payload) {
+    if (payload && typeof payload.targetId == "string") {
+	this._setContent(payload.targetId, payload.value);
+    }
+};
+
+// Callback function to handle resetting tab contents via include.
+// Event payload: {targetId: "<tab_id>", value: "<source_url>"}
+@JMAKI_NS@.accordion.Widget.prototype._setIncludeCallback = function(payload) {
+    if (payload && typeof payload.targetId == "string") {
+	this._setInclude(payload.targetId, payload.value);
+    }
+};
+
+// Callback function to handle resetting tab contents via widge reference.
+// Event payload: {targetId: "<tab_id>", value: "<widget_id>"}
+@JMAKI_NS@.accordion.Widget.prototype._setRefidCallback = function(payload) {
+    if (payload && typeof payload.targetId == "string") {
+	this._setRefid(payload.targetId, payload.value);
+    }
+};
 
 // select a tab
+@JMAKI_NS@.accordion.Widget.prototype._select = function(tabId) {
+    var widget = @JS_NS@.widget.common.getWidget(this._wid);
+    if (widget) {
+	widget.tabSelected({id: tabId});
+    }
+};
+
+// Set the content of the tab with escaped html string.
+@JMAKI_NS@.accordion.Widget.prototype._setContent = function(tabId, content) {
+    var widgetTab = @JS_NS@.widget.common.getWidget(tabId);
+    if (widgetTab && typeof content == "string") {
+	var props = {};
+	var tabContentElement = {html: content};	       
+	props.tabContent = [];
+	props.tabContent.push(tabContentElement);
+	widgetTab.setProps(props);
+    }
+};
+
+// Set the content of the tab with include source.
+@JMAKI_NS@.accordion.Widget.prototype._setInclude = function(tabId, include) {
+    var widgetTab = @JS_NS@.widget.common.getWidget(tabId);
+    if (widgetTab && typeof include == "string") {
+	var props = {};
+	var tabContentElement = {include: include};	       
+	props.tabContent = [];
+	props.tabContent.push(tabContentElement);
+	widgetTab.setProps(props);
+    }
+};
+
+// Set the content of the tab with referenced widget.
+@JMAKI_NS@.accordion.Widget.prototype._setRefid = function(tabId, refid) {
+    var widgetTab = @JS_NS@.widget.common.getWidget(tabId);
+    if (widgetTab && typeof refid == "string") {
+	var props = {};
+	var tabContentElement = {refId: refid};	       
+	props.tabContent = [];
+	props.tabContent.push(tabContentElement);
+	widgetTab.setProps(props);
+    }
+};
+
+// A tab was selected, publish jMaki onSelect event.
+// Event payload: {widgetId: <wid>, type: "onSelect", targetId: "<tab_id>",
+//		   topic: "/woodstock/accordion/select"}
 @JMAKI_NS@.accordion.Widget.prototype._onTabSelected = function(props) {
-    if (!props || !props.id) {
-        return false;
+    if (props && typeof props.id == "string") {
+	jmaki.processActions({
+	    action: "onSelect",
+	    targetId: props.id,
+	    topic: this._publish + "/onSelect",
+	    type: "onSelect",
+	    widgetId: this._wid
+	});
     }
-
-    var payload = {};
-    payload.targetId = props.id;
-    jmaki.publish(this._publish, payload );
 };
-
 
 // Converts jmaki model to props digestable by woodstock accordion widget
+// Each "items" array object is mapped to an accordionTab widget.
+// Within each items object, the property mapping:
+//   id       -> accordionTab.id (default: widgetId + "_tab" + <index>)
+//   label    -> title (default: "Tab <index>"
+//   content  -> tabContents: [{content: <content_value>}]
+//   include  -> tabContents: [{include: <include_url>}]
+//   refId    -> tabContents: [{refId: <refid_value>}]
+//   selected -> selected
+//   lazyLoad -> loadOnSelect
 @JMAKI_NS@.accordion.Widget.prototype._convertModel = function(props){
     
-    
+    props.tabs = [];
     props.toggleControls = true;
-
     if (!props.items) {
-        return false;
-    }
-    
-    props['tabs']  = [];
-    
-    if (this._isArray(props.items)) {
-        //typeof array returns object. see http://javascript.crockford.com/remedial.html
-        for (i = 0; i < props.items.length; i += 1) {
-            var tab  = {};
-           
-            //defaults
-            //tab.contentHeight = "100px"; 
-            tab.widgetType = "accordionTab";
-            tab.id = (typeof props.items[i].id != "undefined" && props.items[i].id != null)? props.items[i].id : this._wid + "_tab" + i;
-            tab.title = (typeof props.items[i].label != "undefined" && props.items[i].label != null)? props.items[i].label : "Tab " + i;
-            
-            tab.tabContent = [];
-                
-            if (typeof props.items[i].include != "undefined" && props.items[i].include != null) {
-                var tabContentElement = {};
-                tabContentElement.include = props.items[i].include;
-                tab.tabContent.push(tabContentElement);
-            }
-            if (props.items[i].content) {
-                var tabContentElement = {};
-                tabContentElement.html = props.items[i].content;
-                tab.tabContent.push(tabContentElement);
-            } 
-            
-            if (typeof props.items[i].selected != "undefined" && props.items[i].selected != null) {
-                //mark tab selected
-                tab.selected = props.items[i].selected;
-            }
-            
-            if (typeof props.items[i].lazyLoad != "undefined" && props.items[i].lazyLoad != null) {
-                //if set to true then the content is loaded when the pane is selected.
-                props.loadOnSelect = props.items[i].lazyLoad;
-            }
-            
-           
-            
-            props["tabs"].push(tab);        
-
-        }
+	return false;
     }
 
-
+    if (props.items instanceof Array) {
+	for (i = 0; i < props.items.length; i += 1) {
+	    var item = props.items[i];
+	    var tab  = {};
+	    tab.widgetType = "accordionTab";
+	    tab.id = (item.id != null) ? item.id : this._wid + "_tab" + i;
+	    tab.title = (item.label != null) ? item.label : "Tab " + i;
+	    tab.tabContent = [];
+	    if (item.include != null) {
+		var tabContentElement = {};
+		tabContentElement.include = item.include;
+		tab.tabContent.push(tabContentElement);
+	    }
+	    if (item.content != null) {
+		var tabContentElement = {};
+		tabContentElement.html = item.content;
+		tab.tabContent.push(tabContentElement);
+	    } 
+	    if (typeof item.refId == "string") {
+		var tabContentElement = {};
+		tabContentElement.refId = item.refId;
+		tab.tabContent.push(tabContentElement);
+	    } 
+	    if (item.selected && item.selected == true) {
+		tab.selected = true;
+	    }
+	    if (item.lazyLoad && item.lazyLoad == true) {
+		props.loadOnSelect = true;
+	    }
+	    props.tabs.push(tab);	
+	}	// End of for
+    }
 };
-
-
-// util method to check whether JSON object is an array
-@JMAKI_NS@.accordion.Widget.prototype._isArray = function(testObject){    
-    // return testObject && !(testObject.propertyIsEnumerable('length')) && typeof testObject === 'object' && typeof testObject.length === 'number';
-    // return (testObject && typeof testObject == "object" && typeof testObject.length != "undefined");
-    return (testObject instanceof Array);
-}
-
--- jmaki/src/templates/accordion/accordion.jsp
+++ jmaki/src/templates/accordion/accordion.jsp
@@ -1,10 +1,5 @@
-  <a:widget name='woodstock.accordion' 
-    value = "{items : 
-           [
-           {id: 'books', label : 'Books', content : 'Book content'},
-           {id: 'music', label : 'Music', content : 'Music content', selected : true},
-           {id : 'magazines', label : 'Magazines', include : 'test.jsp', lazyLoad : true},
-           {label : 'Newspaper', content : 'Newspaper content', selected : false}
+  <a:widget name='woodstock.accordion' value = "{items: [
+           {id: 'tab1', label: 'Tab 1', content: 'Tab 1 content', selected: true}
            ]}"    
     />
 
